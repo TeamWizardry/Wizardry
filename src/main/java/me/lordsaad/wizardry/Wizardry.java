0e89b75e718f0b907c6157a30daa870ecd5068bd
Dylan Greer <greerd248@yahoo.com>
2016-06-18 02:40:51 -0400

    Added config file and mana world gen

--- src/main/java/me/lordsaad/wizardry/Wizardry.java
+++ src/main/java/me/lordsaad/wizardry/Wizardry.java
@@ -56,6 +56,8 @@ public class Wizardry {
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
         
+        Config.initConfig();
+        
         ModItems.init();
         ModBlocks.init();
         Fluids.preInit();
--- src/main/java/me/lordsaad/wizardry/fluid/FluidBlockMana.java
+++ src/main/java/me/lordsaad/wizardry/fluid/FluidBlockMana.java
@@ -1,14 +1,14 @@
 package me.lordsaad.wizardry.fluid;
 
 import me.lordsaad.wizardry.Wizardry;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class FluidBlockMana extends BlockFluidClassic{
 
@@ -17,15 +17,28 @@ public class FluidBlockMana extends BlockFluidClassic{
 	{
 		super(FluidMana.instance, Material.WATER);
 		
-		this.setQuantaPerBlock(4);
+		this.setQuantaPerBlock(6);
 		this.setUnlocalizedName("mana");
 		this.setCreativeTab(Wizardry.tab);
 	}
 	
-	 @Override
-     public EnumBlockRenderType getRenderType(IBlockState state)
-     {
-     	return EnumBlockRenderType.MODEL;
-     }
+
+	@Override
+	public Fluid getFluid()
+	{
+		return FluidMana.instance;
+	}
+
+	
+	@Override
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+		return EnumBlockRenderType.MODEL;
+    }
+	
+	@Override
+	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block neighborBlock)
+	{
+	}
 
 }
