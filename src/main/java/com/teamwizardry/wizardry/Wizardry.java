2d11fb6def56ab9c6377c70a38439de127fee287
Saad <saadodi44@gmail.com>
2016-06-28 23:09:18 +0300

    fixed: librarianlib is now a hard dependency

--- src/main/java/com/teamwizardry/wizardry/Wizardry.java
+++ src/main/java/com/teamwizardry/wizardry/Wizardry.java
@@ -31,7 +31,7 @@ import org.apache.logging.log4j.Logger;
 /**
  * Created by Saad on 6/9/2016.
  */
-@Mod(modid = Wizardry.MODID, version = Wizardry.VERSION, name = Wizardry.MODNAME, useMetadata = true, dependencies="after:librarianlib")
+@Mod(modid = Wizardry.MODID, version = Wizardry.VERSION, name = Wizardry.MODNAME, useMetadata = true, dependencies = "required-after:librarianlib")
 public class Wizardry {
 
     public static final String MODID = "wizardry";
