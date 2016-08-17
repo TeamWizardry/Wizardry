9f9fbbb7d9b0e360195e57f13fcdd28fff1eff60
Pierce Corcoran <pierce@plasticcow.com>
2016-08-16 20:41:40 -0700

    New uniform names

--- src/main/java/com/teamwizardry/wizardry/client/fx/Shaders.java
+++ src/main/java/com/teamwizardry/wizardry/client/fx/Shaders.java
@@ -18,8 +18,8 @@ public class Shaders {
 	
 	public static class BurstShader extends Shader {
 		
-		public FloatTypes.Float1 fanSpeedMin, fanSpeedMax, fanSizeMin, fanSizeMax, fanJitterMin, fanJitterMax;
-		public IntTypes.Int1 fanBladesMin, fanBladesMax, fanCount;
+		public FloatTypes.FloatUniform fanSpeedMin, fanSpeedMax, fanSizeMin, fanSizeMax, fanJitterMin, fanJitterMax;
+		public IntTypes.IntUniform fanBladesMin, fanBladesMax, fanCount;
 
 	    public BurstShader(String vert, String frag) {
 	        super(vert, frag);
