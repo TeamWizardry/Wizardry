ce692b774cfd30accdd394e2f8f0d58ea5dd9ba9
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-01-31 17:42:22 +1300

    Fix NPE in EntityUnicorn#applyEntityAttributes()

--- src/main/java/com/teamwizardry/wizardry/common/entity/EntityUnicorn.java
+++ src/main/java/com/teamwizardry/wizardry/common/entity/EntityUnicorn.java
@@ -62,10 +62,11 @@ public class EntityUnicorn extends EntityHorse {
 
     @Override
     protected void applyEntityAttributes() {
+	super.applyEntityAttributes();
+	this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
 	this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
 	this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
 	this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(5.0D);
-	super.applyEntityAttributes();
     }
 
     @Override
