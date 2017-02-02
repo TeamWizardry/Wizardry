ca67f84830295dc240ba977ab4df0749d1ec7e9b
Saad <saadodi44@gmail.com>
2017-02-02 20:01:41 +0200

    cone shape complete

--- src/main/java/com/teamwizardry/wizardry/api/util/PosUtils.java
+++ src/main/java/com/teamwizardry/wizardry/api/util/PosUtils.java
@@ -3,6 +3,7 @@ package com.teamwizardry.wizardry.api.util;
 import net.minecraft.block.Block;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 /**
@@ -45,5 +46,37 @@ public final class PosUtils {
 			}
 		}
 		return origin;
+	}
+
+	public static boolean isLyingInCone(Vec3d pointToCheck, Vec3d origin, Vec3d endVector, float aperture) {
+		float[] x = new float[]{(float) pointToCheck.xCoord, (float) pointToCheck.yCoord, (float) pointToCheck.zCoord};
+		float[] t = new float[]{(float) origin.xCoord, (float) origin.yCoord, (float) origin.zCoord};
+		float[] b = new float[]{(float) endVector.xCoord, (float) endVector.yCoord, (float) endVector.zCoord};
+
+		float halfAperture = aperture / 2.f;
+		float[] apexToXVect = dif(t, x);
+		float[] axisVect = dif(t, b);
+
+		boolean isInInfiniteCone = dotProd(apexToXVect, axisVect) / magn(apexToXVect) / magn(axisVect)
+				> Math.cos(halfAperture);
+
+		return isInInfiniteCone && dotProd(apexToXVect, axisVect) / magn(axisVect) < magn(axisVect);
+
+	}
+
+	private static float dotProd(float[] a, float[] b) {
+		return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+	}
+
+	private static float[] dif(float[] a, float[] b) {
+		return (new float[]{
+				a[0] - b[0],
+				a[1] - b[1],
+				a[2] - b[2]
+		});
+	}
+
+	private static float magn(float[] a) {
+		return (float) (Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]));
 	}
 }
--- src/main/java/com/teamwizardry/wizardry/common/module/shapes/ModuleShapeCone.java
+++ src/main/java/com/teamwizardry/wizardry/common/module/shapes/ModuleShapeCone.java
@@ -3,18 +3,20 @@ package com.teamwizardry.wizardry.common.module.shapes;
 import com.teamwizardry.librarianlib.client.fx.particle.ParticleBuilder;
 import com.teamwizardry.librarianlib.client.fx.particle.ParticleSpawner;
 import com.teamwizardry.librarianlib.client.fx.particle.functions.InterpFadeInOut;
+import com.teamwizardry.librarianlib.common.util.math.Matrix4;
 import com.teamwizardry.librarianlib.common.util.math.interpolate.StaticInterp;
-import com.teamwizardry.librarianlib.common.util.math.interpolate.position.InterpHelix;
+import com.teamwizardry.librarianlib.common.util.math.interpolate.position.InterpCircle;
+import com.teamwizardry.librarianlib.common.util.math.interpolate.position.InterpLine;
 import com.teamwizardry.wizardry.Wizardry;
 import com.teamwizardry.wizardry.api.Constants;
-import com.teamwizardry.wizardry.api.spell.Attributes;
-import com.teamwizardry.wizardry.api.spell.Module;
-import com.teamwizardry.wizardry.api.spell.ModuleType;
-import com.teamwizardry.wizardry.api.spell.RegisterModule;
+import com.teamwizardry.wizardry.api.spell.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -22,6 +24,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -76,14 +79,43 @@ public class ModuleShapeCone extends Module {
 
 	@Override
 	public boolean run(@NotNull World world, @Nullable EntityLivingBase caster) {
+		if (caster == null) return false;
+		if (nextModule == null) return true;
 
+		double range = 5;
+		if (attributes.hasKey(Attributes.EXTEND)) range += attributes.getDouble(Attributes.EXTEND);
+		float offX = 0.5f * (float) Math.sin(Math.toRadians(-90.0f - caster.rotationYaw));
+		float offZ = 0.5f * (float) Math.cos(Math.toRadians(-90.0f - caster.rotationYaw));
+		Vec3d origin = new Vec3d(offX, caster.getEyeHeight(), offZ).add(caster.getPositionVector());
+
+		for (int i = 0; i < range * 10; i++) {
+			Matrix4 matrix = new Matrix4();
+			Vec3d cross = caster.getLookVec().crossProduct(new Vec3d(0, 1, 0));
+			matrix.rotate(Math.toRadians(ThreadLocalRandom.current().nextDouble(-range * 10, range * 10)), caster.getLookVec());
+			Vec3d normal = matrix.apply(cross).normalize();
+
+			Matrix4 matrix2 = new Matrix4();
+			matrix2.rotate(Math.toRadians(ThreadLocalRandom.current().nextDouble(-range * 10, range * 10)), normal);
+			Vec3d target = matrix2.apply(caster.getLookVec()).scale(ThreadLocalRandom.current().nextDouble(range)).add(origin);
+			if (nextModule instanceof ITargettable) {
+				((ITargettable) nextModule).run(world, caster, target);
+
+				List<Entity> entityList = world.getEntitiesWithinAABBExcludingEntity(caster, new AxisAlignedBB(new BlockPos(target)));
+				if (entityList.isEmpty()) continue;
+				for (Entity entity : entityList) {
+					if (entity == null) continue;
+					((ITargettable) nextModule).run(world, caster, entity);
+				}
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public void runClient(@NotNull World world, @Nullable ItemStack stack, @Nullable EntityLivingBase caster, @NotNull Vec3d pos) {
 		if (caster == null) return;
-		//if (getColor() == null) return;
+		Color color = getColor();
+		if (color == null) color = Color.WHITE;
 		double range = 5;
 		if (attributes.hasKey(Attributes.EXTEND)) range += attributes.getDouble(Attributes.EXTEND);
 		float offX = 0.5f * (float) Math.sin(Math.toRadians(-90.0f - caster.rotationYaw));
@@ -91,11 +123,12 @@ public class ModuleShapeCone extends Module {
 		Vec3d origin = new Vec3d(offX, caster.getEyeHeight(), offZ).add(caster.getPositionVector());
 
 		ParticleBuilder glitter = new ParticleBuilder(25);
-		glitter.setPositionFunction(new InterpHelix(Vec3d.ZERO, caster.getLookVec().scale(range), 0.0f, (float) range, 3f, ThreadLocalRandom.current().nextFloat()));
-		glitter.setAlphaFunction(new InterpFadeInOut(0.3f, 0.3f));
 		glitter.setRender(new ResourceLocation(Wizardry.MODID, Constants.MISC.SPARKLE_BLURRED));
+		glitter.setCollision(true);
 
-		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(origin), 50, (int) (5 * range), (aFloat, particleBuilder) -> {
+		double finalRange = range;
+		Color finalColor = color;
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(origin), (int) (range * 50), 0, (aFloat, particleBuilder) -> {
 			double radius = 0.5;
 			double theta = 2.0f * (float) Math.PI * ThreadLocalRandom.current().nextFloat();
 			double r = radius * ThreadLocalRandom.current().nextFloat();
@@ -103,11 +136,14 @@ public class ModuleShapeCone extends Module {
 			double z = r * MathHelper.sin((float) theta);
 			glitter.setPositionOffset(new Vec3d(x, ThreadLocalRandom.current().nextDouble(0.5), z));
 			glitter.setColor(new Color(
-					Math.min(255, Color.RED.getRed() + ThreadLocalRandom.current().nextInt(20, 50)),
-					Math.min(255, Color.RED.getGreen() + ThreadLocalRandom.current().nextInt(20, 50)),
-					Math.min(255, Color.RED.getBlue() + ThreadLocalRandom.current().nextInt(20, 50)),
-					Color.RED.getAlpha()));
+					Math.min(255, finalColor.getRed() + ThreadLocalRandom.current().nextInt(20, 50)),
+					Math.min(255, finalColor.getGreen() + ThreadLocalRandom.current().nextInt(20, 50)),
+					Math.min(255, finalColor.getBlue() + ThreadLocalRandom.current().nextInt(20, 50)),
+					100));
 			glitter.setScale(1);
+			glitter.setAlphaFunction(new InterpFadeInOut(0.3f, (float) ThreadLocalRandom.current().nextDouble(0.3, 1)));
+			InterpCircle circle = new InterpCircle(caster.getLookVec().scale(finalRange), caster.getLookVec(), (float) finalRange, 1, ThreadLocalRandom.current().nextFloat());
+			glitter.setPositionFunction(new InterpLine(Vec3d.ZERO, circle.get(0)));
 		});
 	}
 
