56a3753d0b17474c9bf99806352a0964056d3ef6
Saad <saadodi44@gmail.com>
2017-02-24 10:54:03 +0200

    working on crappy sliders

--- src/main/java/com/teamwizardry/wizardry/client/gui/book/BookGui.java
+++ src/main/java/com/teamwizardry/wizardry/client/gui/book/BookGui.java
@@ -10,15 +10,21 @@ import com.teamwizardry.librarianlib.client.gui.GuiBase;
 import com.teamwizardry.librarianlib.client.gui.GuiComponent;
 import com.teamwizardry.librarianlib.client.gui.components.ComponentSprite;
 import com.teamwizardry.librarianlib.client.gui.components.ComponentVoid;
+import com.teamwizardry.librarianlib.client.gui.mixin.gl.GlMixin;
 import com.teamwizardry.librarianlib.client.sprite.Sprite;
 import com.teamwizardry.librarianlib.client.sprite.Texture;
+import com.teamwizardry.librarianlib.common.util.math.Vec2d;
 import com.teamwizardry.wizardry.Wizardry;
+import kotlin.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /**
  * Created by LordSaad.
@@ -27,8 +33,8 @@ public class BookGui extends GuiBase {
 
 	public static Texture SPRITE_SHEET = new Texture(new ResourceLocation(Wizardry.MODID, "textures/gui/book/book.png"));
 	private static Sprite background = SPRITE_SHEET.getSprite("background", 145, 179);
-
 	public ComponentVoid mainIndex;
+	private HashMap<ComponentVoid, Pair<Long, Boolean>> sliders = new HashMap<>();
 
 	public BookGui() {
 		super(145, 179);
@@ -73,9 +79,7 @@ public class BookGui extends GuiBase {
 								icon.draw((int) ClientTickHandler.getPartialTicks(), category.getPos().getXi(), category.getPos().getYi(), 32, 32);
 								GlStateManager.popMatrix();
 							});
-							category.BUS.hook(GuiComponent.MouseOverEvent.class, mouseOverEvent -> {
-								// TODO
-							});
+							hookSlider(category, "TEEEEEEEEEEEEEEEEEST IIIIIIIIIIIIIIIIING");
 							final String finalPath = path + chunk.get("link").getAsString();
 							category.BUS.hook(GuiComponent.MouseClickEvent.class, mouseClickEvent -> {
 								Page page = new Page(this, finalPath, background.getWidth(), background.getHeight(), 0);
@@ -90,7 +94,6 @@ public class BookGui extends GuiBase {
 				}
 			}
 		}
-
 	}
 
 	@Override
@@ -98,4 +101,38 @@ public class BookGui extends GuiBase {
 		return true;
 	}
 
+	public void hookSlider(GuiComponent<?> component, String text) {
+		Slider slider = new Slider(text);
+		slider.component.setPos(new Vec2d(-component.getPos().getX(), component.getPos().getY()));
+		slider.component.setEnabled(false);
+		component.add(slider.component);
+		GlMixin.INSTANCE.transform(slider.component).setValue(new Vec3d(slider.component.getPos().getX(), slider.component.getPos().getY(), -10));
+
+		slider.component.BUS.hook(GuiComponent.ComponentTickEvent.class, componentTickEvent -> {
+			if (sliders.containsKey(slider.component)) {
+				slider.component.setVisible(true);
+				double t = (System.currentTimeMillis() - sliders.get(slider.component).getFirst()) / 700.0;
+				if (sliders.get(slider.component).getSecond()) {
+					double x = -130 * MathHelper.sin((float) ((Math.PI * t)));
+					Minecraft.getMinecraft().player.sendChatMessage(x + " - " + MathHelper.sin((float) ((Math.PI * t) + 0.5)) + " - " + t);
+					slider.component.setPos(new Vec2d(x, slider.component.getPos().getY()));
+				} else {
+					double x = -130 * MathHelper.sin((float) ((Math.PI * t) + 0.5));
+					slider.component.setPos(new Vec2d(x, slider.component.getPos().getY()));
+				}
+			} else {
+				slider.component.setVisible(false);
+			}
+		});
+
+		component.BUS.hook(GuiComponent.MouseInEvent.class, mouseOverEvent -> {
+			if (!sliders.containsKey(slider.component))
+				sliders.put(slider.component, new Pair<>(System.currentTimeMillis(), true));
+		});
+
+		component.BUS.hook(GuiComponent.MouseOutEvent.class, mouseOverEvent -> {
+			if (sliders.containsKey(slider.component))
+				sliders.put(slider.component, new Pair<>(System.currentTimeMillis(), false));
+		});
+	}
 }
---
+++ src/main/java/com/teamwizardry/wizardry/client/gui/book/Slider.java
@@ -0,0 +1,52 @@
+package com.teamwizardry.wizardry.client.gui.book;
+
+import com.teamwizardry.librarianlib.client.gui.GuiComponent;
+import com.teamwizardry.librarianlib.client.gui.components.ComponentSprite;
+import com.teamwizardry.librarianlib.client.gui.components.ComponentText;
+import com.teamwizardry.librarianlib.client.gui.components.ComponentVoid;
+import com.teamwizardry.librarianlib.client.sprite.Sprite;
+import com.teamwizardry.librarianlib.client.sprite.Texture;
+import com.teamwizardry.librarianlib.common.util.math.Vec2d;
+import com.teamwizardry.wizardry.Wizardry;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+
+/**
+ * Created by LordSaad.
+ */
+public class Slider {
+
+	private static Texture sliders = new Texture(new ResourceLocation(Wizardry.MODID, "textures/gui/book/sliders.png"));
+	private static Sprite bg = sliders.getSprite("blue", 133, 37);
+
+	public ComponentVoid component;
+
+	public Slider(String text) {
+		component = new ComponentVoid(0, 0, bg.getWidth(), bg.getHeight());
+		component.addTag("slider");
+
+		ComponentSprite compBG = new ComponentSprite(bg, 0, 0, bg.getWidth(), bg.getHeight());
+		component.add(compBG);
+
+		ComponentText compText = new ComponentText(0, 0, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.MIDDLE);
+		compText.getText().setValue(text);
+		compText.getUnicode().setValue(true);
+		compText.getWrap().setValue(120);
+		component.add(compText);
+
+		component.BUS.hook(GuiComponent.ComponentTickEvent.class, componentTickEvent -> {
+			if (component.hasTag("kill")) {
+				if (component.getPos().getXi() < 0)
+					component.setPos(component.getPos().add(5, 0));
+				else component.invalidate();
+
+				return;
+			}
+			if (component.getPos().getXi() > -130) {
+				double t = Math.abs(component.getPos().getXi());
+				double x = (-130) * (MathHelper.sin((float) (t * Math.PI / 2)));
+				component.setPos(new Vec2d(x, 0));
+			}
+		});
+	}
+}
