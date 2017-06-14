7019de09ce98d57aebc2f5fec4a8e3d2778215ed
yrsegal <yrsegal@gmail.com>
2017-06-14 12:29:00 -0400

    boing

--- src/main/java/com/teamwizardry/wizardry/common/block/BlockCloud.java
+++ src/main/java/com/teamwizardry/wizardry/common/block/BlockCloud.java
@@ -3,8 +3,12 @@ package com.teamwizardry.wizardry.common.block;
 import com.teamwizardry.librarianlib.features.base.block.BlockMod;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 
 import javax.annotation.Nonnull;
 
@@ -21,6 +25,26 @@ public class BlockCloud extends BlockMod {
 	@Override
 	public int getLightValue(@Nonnull IBlockState state, IBlockAccess world, @Nonnull BlockPos pos) {
 		return canProduceLight(world, pos) ? 15 : 0;
+	}
+
+	@Override
+	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance) {
+		// NO-OP
+	}
+
+	@Override
+	public boolean addLandingEffects(IBlockState state, WorldServer worldObj, BlockPos blockPosition, IBlockState iblockstate, EntityLivingBase entity, int numberOfParticles) {
+		return true;
+	}
+
+	@Override
+	public void onLanded(World worldIn, Entity entityIn) {
+		if (entityIn.motionY < -0.5) {
+			entityIn.motionY = -entityIn.motionY * 0.625;
+
+			if (!(entityIn instanceof EntityLivingBase))
+				entityIn.motionY *= 0.8;
+		} else entityIn.motionY = 0.0;
 	}
 
 	public boolean canProduceLight(IBlockAccess world, BlockPos pos) {
