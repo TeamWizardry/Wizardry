b81dfc4cebc65b9aad425fe3394ab7647bd1b0e0
InsomniaKitten <admin@designdevils.co.uk>
2017-09-02 22:57:26 +0100

    "Fix" mana magnet in preparation for new assets

--- src/main/java/com/teamwizardry/wizardry/common/block/BlockManaMagnet.java
+++ src/main/java/com/teamwizardry/wizardry/common/block/BlockManaMagnet.java
@@ -3,15 +3,36 @@ package com.teamwizardry.wizardry.common.block;
 import com.teamwizardry.librarianlib.features.base.block.BlockMod;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 /**
  * Created by Saad on 9/4/2016.
  */
 public class BlockManaMagnet extends BlockMod {
 
+	private static final AxisAlignedBB AABB_MANA_MAGNET = new AxisAlignedBB(0.0625, 0, 0.0625, 0.9375, 0.5625, 0.9375);
+
 	public BlockManaMagnet() {
-		super("mana_magnet", Material.IRON);
+		super("mana_magnet", Material.WOOD);
 		setHardness(0.7f);
-		setSoundType(SoundType.METAL);
+		setSoundType(SoundType.WOOD);
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return AABB_MANA_MAGNET;
 	}
 }
