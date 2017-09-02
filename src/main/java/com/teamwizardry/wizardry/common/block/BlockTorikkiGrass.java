4a398469ecd97737a57bb2d18a9ef8ed879429d6
InsomniaKitten <admin@designdevils.co.uk>
2017-09-02 23:12:43 +0100

    Properly implement torikki grass

--- src/main/java/com/teamwizardry/wizardry/common/block/BlockTorikkiGrass.java
+++ src/main/java/com/teamwizardry/wizardry/common/block/BlockTorikkiGrass.java
@@ -3,10 +3,13 @@ package com.teamwizardry.wizardry.common.block;
 import com.teamwizardry.librarianlib.features.base.block.BlockMod;
 import net.minecraft.block.BlockDirt;
 import net.minecraft.block.IGrowable;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -24,6 +27,9 @@ public class BlockTorikkiGrass extends BlockMod implements IGrowable {
 
 	public BlockTorikkiGrass() {
 		super("torikki_grass", Material.GRASS);
+		setSoundType(SoundType.PLANT);
+		setHardness(0.6f);
+		setResistance(3.0f);
 	}
 
 	@Nonnull
@@ -47,6 +53,21 @@ public class BlockTorikkiGrass extends BlockMod implements IGrowable {
 	@Override
 	public boolean canUseBonemeal(@Nonnull World worldIn, @Nonnull Random rand, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
 		return Blocks.GRASS.canUseBonemeal(worldIn, rand, pos, state);
+	}
+
+	@Override
+	public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+		return true;
+	}
+
+	@Override
+	public boolean isToolEffective(String type, IBlockState state) {
+		return "shovel".equals(type);
+	}
+
+	@Override
+	protected ItemStack getSilkTouchDrop(IBlockState state) {
+		return new ItemStack(this);
 	}
 
 	@Override
