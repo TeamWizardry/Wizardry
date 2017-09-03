c0018a9cb42e141eb7ca92fe1e5cfecd4fd0999c
InsomniaKitten <admin@designdevils.co.uk>
2017-09-03 01:08:57 +0100

    OOPS

--- src/main/java/com/teamwizardry/wizardry/common/potion/PotionBase.java
+++ src/main/java/com/teamwizardry/wizardry/common/potion/PotionBase.java
@@ -1,14 +1,5 @@
 package com.teamwizardry.wizardry.common.potion;
 
-/*
- * This file was created at 01:06 on 03 Sep 2017 by InsomniaKitten
- *
- * It is distributed as part of the Wizardry mod.
- * Source code is visible at: https://github.com/InsomniaKitten/Wizardry
- *
- * Copyright (c) InsomniaKitten 2017. All Rights Reserved.
- */
-
 import com.teamwizardry.librarianlib.features.base.PotionMod;
 import com.teamwizardry.librarianlib.features.helpers.VariantHelper;
 import com.teamwizardry.wizardry.Wizardry;
