a1709ea894a594ebd19468156cc95ebac25534e0
Escapee <smartseth@hotmail.com>
2018-01-04 23:57:39 -0800

    Why was this set?

--- src/main/java/com/teamwizardry/wizardry/Wizardry.java
+++ src/main/java/com/teamwizardry/wizardry/Wizardry.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 /**
  * Created by Saad on 6/9/2016.
  */
-@Mod(modid = Wizardry.MODID, version = Wizardry.VERSION, name = Wizardry.MODNAME, useMetadata = true, dependencies = "required-before:librarianlib")
+@Mod(modid = Wizardry.MODID, version = Wizardry.VERSION, name = Wizardry.MODNAME, dependencies = "required-before:librarianlib")
 public class Wizardry {
 
 	public static final String MODID = "wizardry";
