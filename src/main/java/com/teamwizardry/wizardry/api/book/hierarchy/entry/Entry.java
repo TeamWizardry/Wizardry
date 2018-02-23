861c65a6c43fadee4ff12f39c4c722b403a443ec
Demoniaque <saadodi44@gmail.com>
2018-02-23 22:53:27 +0200

    convert to wire's system

--- src/main/java/com/teamwizardry/wizardry/api/book/hierarchy/entry/Entry.java
+++ src/main/java/com/teamwizardry/wizardry/api/book/hierarchy/entry/Entry.java
@@ -1,5 +1,6 @@
 package com.teamwizardry.wizardry.api.book.hierarchy.entry;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -12,7 +13,6 @@ import com.teamwizardry.wizardry.api.book.hierarchy.entry.criterion.game.EntryUn
 import com.teamwizardry.wizardry.api.book.hierarchy.page.Page;
 import com.teamwizardry.wizardry.client.gui.book.ComponentEntryPage;
 import com.teamwizardry.wizardry.client.gui.book.GuiBook;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
@@ -20,7 +20,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author WireSegal
@@ -29,9 +30,14 @@ import java.util.*;
 public class Entry implements IBookElement {
 
 	public static final Map<ResourceLocation, Entry> ENTRIES = Maps.newHashMap();
+
 	public final Category category;
-	private final HashMap<String, TranslatedEntryHolder> translations = Maps.newHashMap();
-	public JsonElement icon = null;
+
+	public final List<Page> pages;
+	public final String titleKey;
+	public final String descKey;
+	public final JsonElement icon;
+
 	public ICriterion criterion = null;
 
 	public boolean isValid = false;
@@ -39,34 +45,36 @@ public class Entry implements IBookElement {
 	public Entry(Category category, String rl, JsonObject json) {
 		ENTRIES.put(new ResourceLocation(rl), this);
 
+		List<Page> pages = Lists.newArrayList();
+		String titleKey = "";
+		String descKey = "";
+		JsonElement icon = new JsonObject();
+
 		this.category = category;
-
 		try {
-			this.icon = json.get("icon");
-
+			titleKey = json.getAsJsonPrimitive("title").getAsString();
+			descKey = json.getAsJsonPrimitive("description").getAsString();
+			icon = json.get("icon");
+			JsonArray allPages = json.getAsJsonArray("content");
+			for (JsonElement pageJson : allPages) {
+				Page page = Page.fromJson(this, pageJson);
+				if (page != null)
+					pages.add(page);
+			}
 			if (json.has("criteria"))
 				criterion = ICriterion.fromJson(json.get("criteria"));
-			else criterion = null;
-
-			if (json.has("en_us")) {
-				for (Map.Entry<String, JsonElement> languageEntrySet : json.entrySet()) {
-					if (!languageEntrySet.getValue().isJsonObject()) continue;
-					JsonObject object = languageEntrySet.getValue().getAsJsonObject();
-
-					translations.put(languageEntrySet.getKey(), new TranslatedEntryHolder(this, object));
-					isValid = true;
-				}
-			} else {
-				translations.put("en_us", new TranslatedEntryHolder(this, json));
-				isValid = true;
-			}
+			isValid = true;
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+
+		this.pages = pages;
+		this.titleKey = titleKey;
+		this.descKey = descKey;
+		this.icon = icon;
 	}
 
 	public boolean isUnlocked(EntityPlayer player) {
-		ICriterion criterion = getCriterion();
 		if (criterion == null) return true;
 
 		boolean unlocked = !MinecraftForge.EVENT_BUS.post(new EntryUnlockedEvent(player, this));
@@ -83,55 +91,5 @@ public class Entry implements IBookElement {
 	@Override
 	public GuiComponent createComponent(GuiBook book) {
 		return new ComponentEntryPage(book, this);
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Nullable
-	public String getTitle() {
-		String lang = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode().toLowerCase(Locale.ROOT);
-
-		TranslatedEntryHolder translatedEntryHolder = translations.getOrDefault(lang, translations.get("en_us"));
-		return translatedEntryHolder == null ? null : translatedEntryHolder.titleKey;
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Nullable
-	public String getDescription() {
-		String lang = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode().toLowerCase(Locale.ROOT);
-
-		TranslatedEntryHolder translatedEntryHolder = translations.getOrDefault(lang, translations.get("en_us"));
-		return translatedEntryHolder == null ? null : translatedEntryHolder.descKey;
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Nullable
-	public List<Page> getPages() {
-		String lang = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode().toLowerCase(Locale.ROOT);
-
-		TranslatedEntryHolder translatedEntryHolder = translations.getOrDefault(lang, translations.get("en_us"));
-		return translatedEntryHolder == null ? null : translatedEntryHolder.pages;
-	}
-
-	@Nullable
-	public ICriterion getCriterion() {
-		return criterion;
-	}
-
-	public class TranslatedEntryHolder {
-
-		private final String titleKey;
-		private final String descKey;
-		private final List<Page> pages = new ArrayList<>();
-
-		public TranslatedEntryHolder(Entry entry, JsonObject object) {
-			titleKey = object.getAsJsonPrimitive("title").getAsString();
-			descKey = object.getAsJsonPrimitive("description").getAsString();
-			JsonArray allPages = object.getAsJsonArray("content");
-			for (JsonElement pageJson : allPages) {
-				Page page = Page.fromJson(entry, pageJson);
-				if (page != null)
-					pages.add(page);
-			}
-		}
 	}
 }
