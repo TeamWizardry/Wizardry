f76b4279fe91db4a5b12db9b5723648beb7f899f
alesto <alesto@ALESTO-PC>
2018-05-05 10:30:26 +0200

    Fixed ConcurrentModificationException in LightningTracker

--- src/main/java/com/teamwizardry/wizardry/common/core/LightningTracker.java
+++ src/main/java/com/teamwizardry/wizardry/common/core/LightningTracker.java
@@ -14,9 +14,7 @@ import java.util.HashMap;
 public class LightningTracker {
 	public static LightningTracker INSTANCE = new LightningTracker();
 
-	private HashMap<Entity, Integer> entityToTicks = new HashMap<>();
-	private HashMap<Entity, Entity> entityToCaster = new HashMap<>();
-	private HashMap<Entity, Double> entityToPotency = new HashMap<>();
+	private HashMap<Entity, TrackingEntry> entityToEntry = new HashMap<>();
 
 	private LightningTracker() {
 		MinecraftForge.EVENT_BUS.register(this);
@@ -25,31 +23,56 @@ public class LightningTracker {
 	public void addEntity(Vec3d origin, Entity target, Entity caster, double potency) {
 		double dist = target.getPositionVector().subtract(origin).lengthVector();
 		int numPoints = (int) (dist * LightningGenerator.POINTS_PER_DIST);
-		entityToTicks.put(target, numPoints);
-		entityToCaster.put(target, caster);
-		entityToPotency.put(target, potency);
+		entityToEntry.put(target, new TrackingEntry(numPoints, dist, caster));
 	}
 
 	@SubscribeEvent
 	public void tick(TickEvent.WorldTickEvent event) {
-		entityToTicks.keySet().removeIf(entity -> {
-			Entity caster = entityToCaster.get(entity);
-			int ticks = entityToTicks.get(entity);
-			double potency = entityToPotency.get(entity);
-
+		entityToEntry.keySet().removeIf(entity -> {
+			TrackingEntry entry = entityToEntry.get(entity);
+			Entity caster = entry.getCaster();
+			int ticks = entry.getTicks();
+			double potency = entry.getPotency();
+			
 			if (ticks > 0) {
-				entityToTicks.put(entity, --ticks);
+				entry.setTicks(ticks - 1);
 				return false;
 			}
 
-			entityToCaster.remove(entity);
-			entityToPotency.remove(entity);
-
-			entity.setFire((int) potency);
+ 			entity.setFire((int) potency);
 			if (caster instanceof EntityPlayer)
 				entity.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) caster), (float) potency);
 			else entity.attackEntityFrom(DamageSource.LIGHTNING_BOLT, (float) potency);
 			return true;
 		});
+	}
+	
+	private static class TrackingEntry {
+		private int ticks;
+		private final double potency;
+		private final Entity caster;
+		
+		TrackingEntry(int ticks, double potency, Entity caster) {
+			super();
+			this.ticks = ticks;
+			this.potency = potency;
+			this.caster = caster;
+		}
+		
+		public final void setTicks( int ticks ) {
+			this.ticks = ticks;
+		}
+		
+		public final int getTicks() {
+			return ticks;
+		}
+
+		public final double getPotency() {
+			return potency;
+		}
+
+		public final Entity getCaster() {
+			return caster;
+		}
 	}
 }
