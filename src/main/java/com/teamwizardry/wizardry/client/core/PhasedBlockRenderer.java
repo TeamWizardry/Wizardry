aee82b5af84ab92109fea20fd5d4614eff4cbe71
yrsegal@gmail.com <yrsegal@gmail.com>
2018-06-23 13:06:09 -0400

    enjoy this much more beautiful thing

--- src/main/java/com/teamwizardry/wizardry/client/core/PhasedBlockRenderer.java
+++ src/main/java/com/teamwizardry/wizardry/client/core/PhasedBlockRenderer.java
@@ -2,7 +2,6 @@ package com.teamwizardry.wizardry.client.core;
 
 import com.teamwizardry.librarianlib.core.client.ClientTickHandler;
 import com.teamwizardry.wizardry.Wizardry;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
@@ -12,6 +11,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -38,18 +38,13 @@ public class PhasedBlockRenderer {
 	}
 
 	public static final float WARP_TIME_PERIOD = 40f;
+	public static final float WARP_SPACE_PERIOD = 2f;
+	public static final float WARP_MAGNITUDE = 0.0625f;
 	public static final int COLOR = 0x28AEB7;
 	public static final float ALPHA = 0.75f;
-	public static final float BASE_ALPHA = 0.625f;
+	public static final float BASE_ALPHA = 0.5f;
 
 	public static BufferBuilder beginRender(float time) {
-		float timeAngle = (float) Math.PI * time / WARP_TIME_PERIOD;
-
-		float colorWarp = ((MathHelper.cos(timeAngle) + 1) / 2 * (1 - BASE_ALPHA) + BASE_ALPHA) / 0xFF;
-
-		float colorR = ((COLOR & 0xFF0000) >> 16) * colorWarp;
-		float colorG = ((COLOR & 0xFF00) >> 8) * colorWarp;
-		float colorB = (COLOR & 0xFF) * colorWarp;
 
 		GlStateManager.enableAlpha();
 		GlStateManager.enableBlend();
@@ -58,14 +53,14 @@ public class PhasedBlockRenderer {
 		GlStateManager.cullFace(GlStateManager.CullFace.BACK);
 		GlStateManager.depthMask(false);
 
-		GlStateManager.color(colorR, colorG, colorB, ALPHA);
+		GlStateManager.color(1, 1, 1, 1);
 		GlStateManager.shadeModel(GL11.GL_SMOOTH);
 
 		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
 		GL14.glBlendEquation(GL14.GL_FUNC_SUBTRACT);
 
 		BufferBuilder buffer = Tessellator.getInstance().getBuffer();
-		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_NORMAL);
+		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
 		buffer.setTranslation(-TileEntityRendererDispatcher.staticPlayerX, -TileEntityRendererDispatcher.staticPlayerY, -TileEntityRendererDispatcher.staticPlayerZ);
 
 		return buffer;
@@ -83,50 +78,33 @@ public class PhasedBlockRenderer {
 		GlStateManager.enableTexture2D();
 	}
 
-	public static void render(World world, BlockPos.MutableBlockPos mut, BlockPos pos, BufferBuilder buffer) {
-		for (EnumFacing facing : EnumFacing.VALUES) {
-			mut.setPos(pos);
-			mut.offset(facing);
-			IBlockState state = world.getBlockState(mut);
-			if (state.getBlock().isAir(state, world, mut) || state.doesSideBlockRendering(world, mut, facing.getOpposite()))
-				renderFace(facing, pos.getX(), pos.getY(), pos.getZ(), buffer);
-		}
+	public static void render(PhaseObject object, float time, BufferBuilder buffer) {
+		float timeAngle = (float) Math.PI / 2 * (time - object.when) / object.expiry;
+
+		float colorWarp = (MathHelper.cos(timeAngle) * (1 - BASE_ALPHA) + BASE_ALPHA) / 0xFF;
+
+		float colorR = ((COLOR & 0xFF0000) >> 16) * colorWarp;
+		float colorG = ((COLOR & 0xFF00) >> 8) * colorWarp;
+		float colorB = (COLOR & 0xFF) * colorWarp;
+
+		for (SurfaceFace side : object.surface) renderFace(side, colorR, colorG, colorB, time, buffer);
 	}
 
-	public static void renderFace(EnumFacing facing, float x, float y, float z, BufferBuilder buffer) {
-		float xNormal = -facing.getFrontOffsetX();
-		float yNormal = -facing.getFrontOffsetY();
-		float zNormal = -facing.getFrontOffsetZ();
+	public static void renderFace(SurfaceFace face, float r, float g, float b, float time, BufferBuilder buffer) {
+		for (float[] vertexData : face.shapeData) {
+			float timeAngle = (float) Math.PI * time / WARP_TIME_PERIOD;
 
-		int direction = -facing.getAxisDirection().getOffset();
+			float periodicValue = MathHelper.sin(timeAngle) * vertexData[6];
+			float magnitude = (periodicValue + 1) * WARP_MAGNITUDE + .001f;
+			float vectorX = magnitude * vertexData[3];
+			float vectorY = magnitude * vertexData[4];
+			float vectorZ = magnitude * vertexData[5];
 
-		float xLocus = x + (1 - xNormal) / 2;
-		float yLocus = y + (1 - yNormal) / 2;
-		float zLocus = z + (1 - zNormal) / 2;
-		float xShear = yNormal - zNormal;
-		float yShear = zNormal - xNormal;
-		float zShear = xNormal - yNormal;
+			float x = vertexData[0] + vectorX;
+			float y = vertexData[1] + vectorY;
+			float z = vertexData[2] + vectorZ;
 
-		float yShearOne = xShear == 0 ? yShear : 0;
-		float yShearTwo = xShear != 0 ? yShear : 0;
-
-		int renderDirection = -direction;
-		if (facing.getAxis().isHorizontal())
-			renderDirection *= -1;
-
-		for (int shearOne = -renderDirection; shearOne >= -1 && shearOne < 2; shearOne += 2 * renderDirection) {
-			for (int shearTwo = shearOne * renderDirection; shearTwo >= -1 && shearTwo < 2; shearTwo -= 2 * shearOne * renderDirection) {
-
-				float directionX = xShear * shearOne;
-				float directionY = yShearOne * shearOne + yShearTwo * shearTwo;
-				float directionZ = zShear * shearTwo;
-
-				float calculatedX = directionX / 2 + xLocus + direction * 0.001f;
-				float calculatedY = directionY / 2 + yLocus + direction * 0.001f;
-				float calculatedZ = directionZ / 2 + zLocus + direction * 0.001f;
-
-				buffer.pos(calculatedX, calculatedY, calculatedZ).normal(-xNormal, -yNormal, -zNormal).endVertex();
-			}
+			buffer.pos(x, y, z).tex(0, 0).color(r, g, b, ALPHA).normal(face.xNormal, face.yNormal, face.zNormal).endVertex();
 		}
 	}
 
@@ -138,18 +116,14 @@ public class PhasedBlockRenderer {
 
 		Set<PhaseObject> tmp = new HashSet<>(phaseObjects);
 
-		BlockPos.MutableBlockPos mut = new BlockPos.MutableBlockPos();
-
 		for (PhaseObject phaseObject : tmp) {
 			if (Minecraft.getMinecraft().world.getTotalWorldTime() - phaseObject.lastWorldTick > phaseObject.expiry)
 				phaseObjects.remove(phaseObject);
 
-
 			float time = ClientTickHandler.getTicksInGame() +
 					(Minecraft.getMinecraft().isGamePaused() ? 0 : ClientTickHandler.getPartialTicks());
 			BufferBuilder bufferBuilder = beginRender(time);
-			for (BlockPos pos : phaseObject.blocks)
-				render(phaseObject.world, mut, pos, bufferBuilder);
+			render(phaseObject, time, bufferBuilder);
 			finishRender(bufferBuilder);
 		}
 	}
@@ -158,21 +132,108 @@ public class PhasedBlockRenderer {
 	private static class PhaseObject {
 
 		public final long lastWorldTick;
+		public final float when;
 		public final int expiry;
-		private final Set<BlockPos> blocks;
-		private final World world;
+
+		private Set<SurfaceFace> surface;
 
 		public PhaseObject(World world, Set<BlockPos> blocks, int expiry) {
-			this.blocks = blocks;
-			this.world = world;
+			this.surface = SurfaceFace.fromPlaces(blocks);
 			this.lastWorldTick = world.getTotalWorldTime();
 			this.expiry = expiry;
-			init();
+			this.when = ClientTickHandler.getTicksInGame();
+		}
+	}
+
+	private static class SurfaceFace {
+		public final EnumFacing face;
+		public final BlockPos position;
+		public final float[][] shapeData = new float[4][7];
+
+		public final float xNormal, yNormal, zNormal;
+
+		public SurfaceFace(EnumFacing face, BlockPos position, Vec3d shapeLocus) {
+			this.face = face;
+			this.position = position;
+
+			xNormal = -face.getFrontOffsetX();
+			yNormal = -face.getFrontOffsetY();
+			zNormal = -face.getFrontOffsetZ();
+
+			int direction = -face.getAxisDirection().getOffset();
+			int renderDirection = face.getAxis().isHorizontal() ? direction : -direction;
+
+			float xLocus = position.getX() + (1 - xNormal) / 2;
+			float yLocus = position.getY() + (1 - yNormal) / 2;
+			float zLocus = position.getZ() + (1 - zNormal) / 2;
+			float xShear = yNormal - zNormal;
+			float yShear = zNormal - xNormal;
+			float zShear = xNormal - yNormal;
+
+			float yShearOne = xShear == 0 ? yShear : 0;
+			float yShearTwo = xShear != 0 ? yShear : 0;
+
+
+			int idx = 0;
+			for (int shearOne = -renderDirection; shearOne >= -1 && shearOne < 2; shearOne += 2 * renderDirection) {
+				for (int shearTwo = shearOne * renderDirection; shearTwo >= -1 && shearTwo < 2; shearTwo -= 2 * shearOne * renderDirection) {
+
+					float directionX = xShear * shearOne;
+					float directionY = yShearOne * shearOne + yShearTwo * shearTwo;
+					float directionZ = zShear * shearTwo;
+
+					float calculatedX = directionX / 2 + xLocus;
+					float calculatedY = directionY / 2 + yLocus;
+					float calculatedZ = directionZ / 2 + zLocus;
+
+					float distanceToLocusX = (float) shapeLocus.x - calculatedX;
+					float distanceToLocusY = (float) shapeLocus.y - calculatedY;
+					float distanceToLocusZ = (float) shapeLocus.z - calculatedZ;
+
+					float descale = distanceToLocusX * distanceToLocusX +
+							distanceToLocusY * distanceToLocusY +
+							distanceToLocusZ * distanceToLocusZ;
+
+					float positionWarp = MathHelper.sin((calculatedX + calculatedY + calculatedZ) *
+							(float) Math.PI / WARP_SPACE_PERIOD);
+
+					shapeData[idx++] = new float[] { calculatedX, calculatedY, calculatedZ,
+							distanceToLocusX / descale, distanceToLocusY / descale, distanceToLocusZ / descale, positionWarp };
+				}
+			}
 		}
 
-		private void init() {
-			Set<Long> longs = new HashSet<>();
-			for (BlockPos pos : blocks) longs.add(pos.toLong());
+		private static BlockPos.MutableBlockPos VISITOR = new BlockPos.MutableBlockPos();
+
+		public static Set<SurfaceFace> fromPlaces(Set<BlockPos> places) {
+			Set<SurfaceFace> faces = new HashSet<>();
+
+			if (places.isEmpty())
+				return faces;
+
+			double total = places.size();
+			int xLocus = 0, yLocus = 0, zLocus = 0;
+
+			for (BlockPos pos : places) {
+				xLocus += pos.getX();
+				yLocus += pos.getY();
+				zLocus += pos.getZ();
+			}
+
+			Vec3d locus = new Vec3d(xLocus / total, yLocus / total, zLocus / total);
+
+			for (BlockPos pos : places) {
+				for (EnumFacing facing : EnumFacing.VALUES) {
+					VISITOR.setPos(pos.getX() + facing.getFrontOffsetX(),
+							pos.getY() + facing.getFrontOffsetY(),
+							pos.getZ() + facing.getFrontOffsetZ());
+
+					if (!places.contains(VISITOR))
+						faces.add(new SurfaceFace(facing, pos, locus));
+				}
+			}
+
+			return faces;
 		}
 	}
 }
