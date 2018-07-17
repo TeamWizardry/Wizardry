e061f3759a52cc6d17f76bf3c3b5a785dc3724e5
Demoniaque <saadodi44@gmail.com>
2018-07-17 20:42:38 +0300

    adjust particles of time slow a little + add to dev changelog

--- src/main/java/com/teamwizardry/wizardry/common/module/effects/ModuleEffectTimeSlow.java
+++ src/main/java/com/teamwizardry/wizardry/common/module/effects/ModuleEffectTimeSlow.java
@@ -104,24 +104,25 @@ public class ModuleEffectTimeSlow extends ModuleEffect {
 		ParticleBuilder glitter = new ParticleBuilder(30);
 		glitter.setColorFunction(new InterpColorHSV(getPrimaryColor(), getSecondaryColor()));
 		glitter.setRender(new ResourceLocation(Wizardry.MODID, Constants.MISC.SPARKLE_BLURRED));
-		glitter.disableRandom();
 		glitter.setScaleFunction(new InterpScale(1, 0));
 		glitter.setCollision(true);
 		glitter.enableMotionCalculation();
-		glitter.setAcceleration(new Vec3d(0, -0.001, 0));
+		glitter.setAcceleration(new Vec3d(0, RandUtil.nextBoolean() ? -0.0001 : 0.0001, 0));
+		glitter.disableRandom();
 
-		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(position.addVector(0, 1, 0)), 3, 0, (aFloat, particleBuilder) -> {
-			glitter.setLifetime(RandUtil.nextInt(30, 40));
-			glitter.setScale(RandUtil.nextFloat());
-			glitter.setAlphaFunction(new InterpFadeInOut(0.5f, RandUtil.nextFloat()));
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(position), 5, 0, (aFloat, particleBuilder) -> {
+			glitter.setLifetime(RandUtil.nextInt(40, 80));
+			glitter.setScaleFunction(new InterpScale(RandUtil.nextFloat(0.5f, 1f), 0f));
+			glitter.setAlphaFunction(new InterpFadeInOut(0.5f, 0.5f));
 
-			double radius = RandUtil.nextDouble(0, 1);
+			double radius = RandUtil.nextDouble(0, 2);
 			double theta = 2.0f * (float) Math.PI * RandUtil.nextFloat();
 			double r = radius * RandUtil.nextFloat();
 			double x = r * MathHelper.cos((float) theta);
 			double z = r * MathHelper.sin((float) theta);
 			Vec3d dest = new Vec3d(x, RandUtil.nextDouble(-radius, radius), z);
 			glitter.setPositionOffset(dest);
+			glitter.setMotion(new Vec3d(RandUtil.nextDouble(-0.001, 0.001), 0, RandUtil.nextDouble(-0.001, 0.001)));
 
 			//glitter.setPositionFunction(new InterpSlowDown(Vec3d.ZERO, new Vec3d(0, RandUtil.nextDouble(-1, 1), 0)));
 			//glitter.setPositionFunction(new InterpBezier3D(Vec3d.ZERO, position.subtract(dest), dest.scale(2), new Vec3d(position.x, radius, position.z)));
