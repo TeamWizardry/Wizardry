421d45081da4c46a8b83ebafb42c325e3272187d
Demoniaque <saadodi44@gmail.com>
2019-06-16 18:30:43 +0300

    extraction spell now succs entities in a different way (needs docs) and respects locked chests

--- src/main/java/com/teamwizardry/wizardry/common/module/effects/ModuleEffectExtract.java
+++ src/main/java/com/teamwizardry/wizardry/common/module/effects/ModuleEffectExtract.java
@@ -6,17 +6,20 @@ import com.teamwizardry.wizardry.api.spell.annotation.RegisterModule;
 import com.teamwizardry.wizardry.api.spell.attribute.AttributeRegistry;
 import com.teamwizardry.wizardry.api.spell.module.IModuleEffect;
 import com.teamwizardry.wizardry.api.spell.module.ModuleInstanceEffect;
+import com.teamwizardry.wizardry.api.util.RandUtil;
 import com.teamwizardry.wizardry.client.fx.LibParticles;
 import com.teamwizardry.wizardry.init.ModSounds;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.ILockableContainer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -37,32 +40,111 @@ public class ModuleEffectExtract implements IModuleEffect {
 		return new String[]{"modifier_increase_potency", "modifier_extend_time"};
 	}
 
+	private static boolean shouldContinue(Entity caster, EntityLivingBase target, ItemStack stack) {
+		if (!(caster instanceof EntityLivingBase)) return true;
+
+		ItemStack held = ((EntityLivingBase) caster).getHeldItemMainhand();
+
+		return !target.getUniqueID().equals(caster.getUniqueID()) || !ItemStack.areItemStacksEqual(held, stack);
+	}
+
 	@Override
 	public boolean run(@NotNull World world, ModuleInstanceEffect instance, @Nonnull SpellData spell, @Nonnull SpellRing spellRing) {
+		Entity caster = spell.getCaster(world);
 		Entity targetEntity = spell.getVictim(world);
 		BlockPos targetPos = spell.getTargetPos();
 		EnumFacing facing = spell.getFaceHit();
 
+		if (caster == null) return true;
+
 		double potency = spellRing.getAttributeValue(world, AttributeRegistry.POTENCY, spell);
+		double maxPotency = spellRing.getModule() != null ? spellRing.getModule().getAttributeRanges().get(AttributeRegistry.POTENCY).max : 0;
+		float powerLevel = (float) (potency / maxPotency);
 		double duration = spellRing.getAttributeValue(world, AttributeRegistry.DURATION, spell);
 
 		if (targetEntity instanceof EntityLivingBase) {
-			if (!world.isRemote) {
-				if (!spellRing.taxCaster(world, spell, true)) return false;
+			if (!spellRing.taxCaster(world, spell, true)) return false;
 
-				ItemStack held = ((EntityLivingBase) targetEntity).getHeldItemMainhand();
+			ItemStack stack = ItemStack.EMPTY;
 
-				EntityItem entityitem = new EntityItem(world, targetEntity.posX, targetEntity.posY, targetEntity.posZ, held.copy());
-				entityitem.setPickupDelay((int) (duration));
-				world.playSound(null, targetEntity.getPosition(), ModSounds.ELECTRIC_BLAST, SoundCategory.NEUTRAL, 1, 1);
-				world.spawnEntity(entityitem);
-				held.setCount(0);
+			boolean success = false;
 
-				return true;
+			if (!(targetEntity instanceof EntityPlayer)) {
+				stack = ((EntityLivingBase) targetEntity).getHeldItemMainhand();
+				if (stack.isEmpty()) {
+					stack = ((EntityLivingBase) targetEntity).getHeldItemOffhand();
+				}
 			}
+			if (!stack.isEmpty()) success = true;
+
+			if (!success)
+				if (powerLevel <= 0.25) {
+					// back to front of inv
+					if (targetEntity instanceof EntityPlayer) {
+						for (int i = ((EntityPlayer) targetEntity).inventory.getSizeInventory() - 1; i > 0; i--) {
+							ItemStack invStack = ((EntityPlayer) targetEntity).inventory.getStackInSlot(i);
+							if (!invStack.isEmpty() && shouldContinue(caster, (EntityLivingBase) targetEntity, invStack)) {
+								stack = invStack;
+								success = true;
+								break;
+							}
+						}
+					}
+				} else if (powerLevel <= 0.5) {
+					// front to back of inv
+					if (targetEntity instanceof EntityPlayer) {
+						for (int i = 0; i < ((EntityPlayer) targetEntity).inventory.getSizeInventory(); i++) {
+							ItemStack invStack = ((EntityPlayer) targetEntity).inventory.getStackInSlot(i);
+							if (!invStack.isEmpty() && shouldContinue(caster, (EntityLivingBase) targetEntity, invStack)) {
+								stack = invStack;
+								success = true;
+								break;
+							}
+						}
+					}
+				} else {
+					// front to back with armor
+					if (targetEntity instanceof EntityPlayer) {
+						for (int i = 0; i < ((EntityPlayer) targetEntity).inventory.getSizeInventory(); i++) {
+							ItemStack invStack = ((EntityPlayer) targetEntity).inventory.getStackInSlot(i);
+							if (!invStack.isEmpty() && shouldContinue(caster, (EntityLivingBase) targetEntity, invStack)) {
+								stack = invStack;
+								success = true;
+								break;
+							}
+						}
+					}
+
+					if (!success) {
+						for (ItemStack armorStack : targetEntity.getArmorInventoryList()) {
+							if (!armorStack.isEmpty()) {
+
+								stack = armorStack;
+								success = true;
+								break;
+							}
+						}
+					}
+				}
+
+			if (!success || stack.isEmpty())
+				return true;
+
+			ItemStack left = stack.splitStack(Math.max((int) (stack.getMaxStackSize() * powerLevel), 1));
+
+			EntityItem entityitem = new EntityItem(world, targetEntity.posX, targetEntity.posY, targetEntity.posZ, left);
+			entityitem.setPickupDelay((int) (duration));
+			world.playSound(null, targetEntity.getPosition(), ModSounds.ENCHANTED_WHASHOOSH, SoundCategory.NEUTRAL, 1, RandUtil.nextFloat(0.5f, 1.5f));
+			world.spawnEntity(entityitem);
+
+
+			return true;
+
 		} else if (targetPos != null) {
 			TileEntity tile = world.getTileEntity(targetPos);
 			if (tile != null) {
+				if (tile instanceof ILockableContainer && ((ILockableContainer) tile).isLocked()) return true;
+
 				if (tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing)) {
 					IItemHandler cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing);
 					if (cap == null) return true;
