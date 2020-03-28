0e4e6693f7e6829daac82c07ee35462567b85b62
Kamefrede <kamefrede@gmail.com>
2020-03-28 23:13:05 +0000

    Functional ModuleLoader.java

--- src/main/java/com/teamwizardry/wizardry/Wizardry.java
+++ src/main/java/com/teamwizardry/wizardry/Wizardry.java
@@ -1,36 +1,41 @@
 package com.teamwizardry.wizardry;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.teamwizardry.wizardry.api.spell.Pattern;
 import com.teamwizardry.wizardry.common.init.PatternInit;
 import com.teamwizardry.wizardry.common.spell.ModuleLoader;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.IResourceManager;
+import net.minecraft.resources.IReloadableResourceManager;
+import net.minecraft.resources.IResourceManagerReloadListener;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegistryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Wizardry.MODID)
 public class Wizardry
 {
-	private static final Logger LOGGER = LogManager.getLogger();
-	
+
+
 	public static final String MODID = "wizardry";
+	private static final Logger LOGGER = LogManager.getLogger(MODID);
+	public Wizardry INSTANCE;
+
 	
 	public Wizardry()
 	{
+		INSTANCE = this;
 	    IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 	    eventBus.addListener(this::init);
 	    eventBus.addListener(this::registerRegistries);
+		MinecraftForge.EVENT_BUS.addListener(this::serverStartingEvent);
 	    eventBus.addGenericListener(Pattern.class, this::registerPatterns);
+
 	}
 	
 	private void registerRegistries(RegistryEvent.NewRegistry event)
@@ -45,16 +50,14 @@ public class Wizardry
 	{
 	    PatternInit.init(event.getRegistry());
 	}
+
+	public void serverStartingEvent(FMLServerAboutToStartEvent event){
+		IReloadableResourceManager manager = event.getServer().getResourceManager();
+		manager.addReloadListener((IResourceManagerReloadListener) (listener) -> ModuleLoader.loadModules(manager));
+	}
 	
 	public void init(final FMLCommonSetupEvent event)
 	{
 		LOGGER.info("Initializing!");
-		/* Let's leave this until we need to do things in-game */
-		DeferredWorkQueue.runLater(() -> {
-		    LOGGER.info("Loading Modules");
-		    IResourceManager resourceManager = Minecraft.getInstance().getResourceManager();
-		    ModuleLoader.loadModules(resourceManager);
-		    LOGGER.info("Finished loading Modules");
-		});
 	}
 }
--- src/main/java/com/teamwizardry/wizardry/common/spell/ModuleLoader.java
+++ src/main/java/com/teamwizardry/wizardry/common/spell/ModuleLoader.java
@@ -1,34 +1,31 @@
 package com.teamwizardry.wizardry.common.spell;
 
-import java.awt.Color;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
+import com.google.common.base.Joiner;
+import com.teamwizardry.wizardry.Wizardry;
+import com.teamwizardry.wizardry.api.spell.*;
+import net.minecraft.item.Item;
+import net.minecraft.resources.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.loading.moddiscovery.ModFile;
+import net.minecraftforge.fml.packs.ModFileResourcePack;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.commons.lang3.Range;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.yaml.snakeyaml.Yaml;
 
-import com.teamwizardry.wizardry.Wizardry;
-import com.teamwizardry.wizardry.api.spell.Module;
-import com.teamwizardry.wizardry.api.spell.ModuleEffect;
-import com.teamwizardry.wizardry.api.spell.ModuleShape;
-import com.teamwizardry.wizardry.api.spell.Pattern;
-import com.teamwizardry.wizardry.api.spell.PatternEffect;
-
-import net.minecraft.item.Item;
-import net.minecraft.resources.IResource;
-import net.minecraft.resources.IResourceManager;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.registries.ForgeRegistries;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Handles loading Modules from yaml resources. Relies heavily on a cohesive
@@ -80,7 +77,7 @@ public class ModuleLoader
     private static final Yaml yaml = new Yaml();
     private static final Logger LOGGER = LogManager.getLogger();
     
-    private static final String folder = Wizardry.MODID + "/module";
+    private static final String folder =  Wizardry.MODID + "/module";
     
     /**
      * Unconstructable
@@ -92,26 +89,46 @@ public class ModuleLoader
      * 
      * @see #loadModules(InputStream, Function, Function)
      */
-    public static void loadModules(IResourceManager resourceManager)
+    public static void loadModules(IReloadableResourceManager resourceManager)
     {
+        if(!(resourceManager instanceof SimpleReloadableResourceManager))
+            return;
         List<Module> shapes = new LinkedList<>();
-        
-        for (ResourceLocation file : resourceManager.getAllResourceLocations(folder, n -> n.endsWith(".yaml")))
-        {
-            try
-            {
-                for (IResource resource : resourceManager.getAllResources(file))
-                {
-                    LOGGER.info("Found Module file: " + file.getPath());
-                    shapes.addAll(loadModules(resource.getInputStream(),
-                                              GameRegistry.findRegistry(Pattern.class)::getValue,
-                                              ForgeRegistries.ITEMS::getValue));
+        for (String resourceNamespace : resourceManager.getResourceNamespaces()) {
+            if(resourceNamespace.equals("minecraft") || resourceNamespace.equals("forge"))
+                continue;
+            FallbackResourceManager fallbackResourceManager = ((SimpleReloadableResourceManager) resourceManager).namespaceResourceManagers.get(resourceNamespace);
+            for (IResourcePack pack : fallbackResourceManager.resourcePacks){
+                if(pack.getResourceNamespaces(ResourcePackType.SERVER_DATA).isEmpty() || !(pack instanceof ResourcePack))
+                    continue;
+                if(pack instanceof ModFileResourcePack){
+                    for (ResourceLocation resourceLocation : loadModulesFromModFile(ResourcePackType.SERVER_DATA, folder, Integer.MAX_VALUE, file -> file.endsWith(".yaml"), ((ModFileResourcePack) pack).getModFile())) {
+                        LOGGER.info("Found Module file: " + resourceLocation);
+                        try {
+                            shapes.addAll(loadModules(pack.getResourceStream(ResourcePackType.SERVER_DATA, resourceLocation),
+                                    GameRegistry.findRegistry(Pattern.class)::getValue,
+                                    ForgeRegistries.ITEMS::getValue));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    for (ResourceLocation resourceLocation :  pack.getAllResourceLocations(ResourcePackType.SERVER_DATA, (String) pack.getResourceNamespaces(ResourcePackType.SERVER_DATA).toArray()[0], folder, Integer.MAX_VALUE, file -> file.endsWith(".yaml"))) {
+                        LOGGER.info("Found Module file: " + resourceLocation);
+                        try {
+                            shapes.addAll(loadModules(pack.getResourceStream(ResourcePackType.SERVER_DATA, resourceLocation),
+                                    GameRegistry.findRegistry(Pattern.class)::getValue,
+                                    ForgeRegistries.ITEMS::getValue));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
                 }
+
+
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+
         }
         shapes.forEach(LOGGER::info);
     }
@@ -176,5 +193,42 @@ public class ModuleLoader
         }
         // Only pattern types are Shapes and Effects, so if not an Effect...
         return new ModuleShape(pattern, name, item, attributeRanges, tags, hiddenTags);
+    }
+
+    /**
+     * Copy of {@link ModFileResourcePack#getAllResourceLocations(ResourcePackType, String, String, int, Predicate)}
+     * but seeing as that does not work well with a pathIn with subfolders, this is necessary
+     *
+     * @param type If it's either a data folder {@link ResourcePackType#SERVER_DATA} or an assets folder {@link ResourcePackType#CLIENT_RESOURCES}
+     * @param pathIn the path in which to look for Resource Locations
+     * @param maxDepth the amount of recursion in regards to subfolders
+     * @param filter A predicate to test the file name against
+     * @param modFile The Mod file in which to search
+     * @return A {@link Collection<ResourceLocation>} constructed from the values in the yaml
+     */
+    public static Collection<ResourceLocation> loadModulesFromModFile(ResourcePackType type, String pathIn, int maxDepth, Predicate<String> filter, ModFile modFile)
+    {
+        try
+        {
+            Path root = modFile.getLocator().findPath(modFile, type.getDirectoryName()).toAbsolutePath();
+            Path inputPath = root.getFileSystem().getPath(pathIn);
+            return Files.walk(root).
+                    map(path -> root.relativize(path.toAbsolutePath())).
+                    filter(path -> path.getNameCount() > 1 && path.getNameCount() - 1 <= maxDepth). // Make sure the depth is within bounds, ignoring domain
+                    filter(path -> !path.toString().endsWith(".mcmeta")). // Ignore .mcmeta files
+                    filter(path -> path.toString().startsWith(inputPath.toString())). // Make sure the target path is inside this one (again ignoring domain)
+                    filter(path -> filter.test(path.getFileName().toString())). // Test the file name against the predicate
+                    filter(path -> path.toString().startsWith(inputPath.toString())).
+                    // Finally we need to form the RL, so use the first name as the domain, and the rest as the path
+                    // It is VERY IMPORTANT that we do not rely on Path.toString as this is inconsistent between operating systems
+                    // Join the path names ourselves to force forward slashes
+                    //
+                    map(path -> new ResourceLocation(path.getName(0).toString(), Joiner.on('/').join(path.subpath(1,Math.min(maxDepth, path.getNameCount()))))).
+                    collect(Collectors.toList());
+        }
+        catch (IOException e)
+        {
+            return Collections.emptyList();
+        }
     }
 }
