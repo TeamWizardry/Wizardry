2677734af9a6916fe4512559f742182503826617
Demoniaque <saadodi44@gmail.com>
2020-11-20 18:15:13 +0200

    increase minimum modifier range to player default reach (5) and switch casting from left click to right click

--- src/main/java/com/teamwizardry/wizardry/common/item/ItemStaff.java
+++ src/main/java/com/teamwizardry/wizardry/common/item/ItemStaff.java
@@ -4,12 +4,14 @@ import com.teamwizardry.wizardry.api.StringConsts;
 import com.teamwizardry.wizardry.api.item.INacreProduct;
 import com.teamwizardry.wizardry.api.spell.Interactor;
 import com.teamwizardry.wizardry.common.spell.SpellCompiler;
-
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 
 /**
  * Project: Wizardry
@@ -21,16 +23,18 @@ public class ItemStaff extends Item implements INacreProduct.INacreDecayProduct 
 		super(properties);
 		this.addPropertyOverride(new ResourceLocation("staff_state"), (stack, world, entity) -> stack.hasTag() && stack.getTag().contains(StringConsts.SPELL_DATA) ? 1 : 0);
 	}
-	
-	public boolean onEntitySwing(ItemStack stack, LivingEntity entity)
-	{
-	    // FIXME: Test Spell
-	    Interactor caster = new Interactor(entity);
-	    SpellCompiler.get()
-	                 .compileSpell(new ItemStack(Items.BEEF), new ItemStack(Items.PORKCHOP))
-	                 .toInstance(caster)
-	                 .run(entity.getEntityWorld(), caster);
-	    
-	    return true;
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+
+		Interactor caster = new Interactor(player);
+		SpellCompiler.get()
+				.compileSpell(new ItemStack(Items.BEEF), new ItemStack(Items.PORKCHOP))
+				.toInstance(caster)
+				.run(world, caster);
+
+		player.setActiveHand(hand);
+
+		return super.onItemRightClick(world, player, hand);
 	}
 }
