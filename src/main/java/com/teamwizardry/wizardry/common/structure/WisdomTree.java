22616d37bdca09880a6f43addcb492c84772b4e3
Demoniaque <saadodi44@gmail.com>
2020-11-26 14:42:43 +0200

    sapling is better at pretending to be a sapling

--- src/main/java/com/teamwizardry/wizardry/common/structure/WisdomTree.java
+++ src/main/java/com/teamwizardry/wizardry/common/structure/WisdomTree.java
@@ -1,13 +1,11 @@
 package com.teamwizardry.wizardry.common.structure;
 
 import com.teamwizardry.wizardry.common.init.ModBlocks;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.trees.OakTree;
 import net.minecraft.block.trees.Tree;
-import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
 import net.minecraftforge.common.IPlantable;
@@ -21,20 +19,20 @@ import java.util.Random;
  * Copyright (c) Carbon 2020
  */
 public class WisdomTree extends Tree {
-	public static final TreeFeatureConfig WISDOM_TREE_CONFIG =
-			(new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(ModBlocks.wisdomLog.get().getDefaultState()),
-					new SimpleBlockStateProvider(ModBlocks.wisdomLeaves.get().getDefaultState()),
-					new BlobFoliagePlacer(2, 0)))
-			.baseHeight(5)
-			.heightRandA(2)
-			.foliageHeight(3)
-			.ignoreVines()
-			.setSapling((IPlantable) ModBlocks.wisdomSapling.get()).build();
+    public static final TreeFeatureConfig WISDOM_TREE_CONFIG = (new TreeFeatureConfig.Builder(
+            new SimpleBlockStateProvider(ModBlocks.wisdomLog.get().getDefaultState()),
+            new SimpleBlockStateProvider(ModBlocks.wisdomLeaves.get().getDefaultState()),
+            new BlobFoliagePlacer(2, 0)))
+            .baseHeight(5)
+            .heightRandA(2)
+            .foliageHeight(3)
+            .ignoreVines()
+            .setSapling((IPlantable) ModBlocks.wisdomSapling.get())
+            .build();
 
-
-	@Nullable
-	@Override
-	protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean hasNearbyFlora) {
-		return Feature.NORMAL_TREE.withConfiguration(WISDOM_TREE_CONFIG);
-	}
+    @Nullable
+    @Override
+    protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean hasNearbyFlora) {
+        return Feature.NORMAL_TREE.withConfiguration(WISDOM_TREE_CONFIG);
+    }
 }
