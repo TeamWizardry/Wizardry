132f72836e32190e1c35ceb57a0ab20ebaa8eb80
Calum <calumgcoppack@gmail.com>
2020-12-24 13:03:40 -0500

    I made mana battery extend the wrong kind of block

--- src/main/java/com/teamwizardry/wizardry/common/block/BlockManaBattery.java
+++ src/main/java/com/teamwizardry/wizardry/common/block/BlockManaBattery.java
@@ -1,8 +1,6 @@
 package com.teamwizardry.wizardry.common.block;
 
-import jdk.nashorn.internal.ir.Block;
-import jdk.nashorn.internal.ir.Statement;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Block;
 
 /**
  * Project: Wizardry
@@ -10,7 +8,7 @@ import net.minecraft.block.Blocks;
  * Copyright (c) Carbon 2020
  */
 public class BlockManaBattery extends Block {
-	public BlockManaBattery(long token, int finish, Statement... statements) {
-		super(token, finish, statements);
+	public BlockManaBattery(Properties properties) {
+		super(properties);
 	}
 }
