685e8e0c84afa739d15f75eff9ee3cf8fb3fa488
Calum <calumgcoppack@gmail.com>
2021-01-08 23:11:29 -0500

    Fixed logger bug

--- src/main/java/com/teamwizardry/wizardry/api/WizConsts.java
+++ src/main/java/com/teamwizardry/wizardry/api/WizConsts.java
@@ -9,7 +9,7 @@ public class WizConsts
     private static CourierChannel courier;
     private static Logger logger;
     
-    public static void setLogger(Logger logger) { if (WizConsts.logger != null) WizConsts.logger = logger; }
+    public static void setLogger(Logger logger) { if (WizConsts.logger == null) WizConsts.logger = logger; }
     public static Logger getLogger() { return logger; }
     
     public static void setCourier(CourierChannel courier) { if (WizConsts.courier == null) WizConsts.courier = courier; }
